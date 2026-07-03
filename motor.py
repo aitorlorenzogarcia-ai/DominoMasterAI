07e85b29356b26efb567208d6d7513894caa1d8a
Aitor Lorenzo <aitorlorenzogarcia@gmail.com>
2026-07-04 00:01:54 +0100

    Motor: primeras jugadas posibles

--- motor.py
+++ motor.py
@@ -1,1 +1,32 @@
-# Aquí irá toda la inteligencia artificial
+class Motor:
+
+    def __init__(self, partida):
+
+        self.partida = partida
+
+    def jugadas_posibles(self, jugador):
+
+        posibles = []
+
+        tablero = self.partida.tablero
+
+        if tablero.esta_vacio():
+
+            return jugador.mano.copy()
+
+        izquierda = tablero.izquierda().izquierda
+
+        derecha = tablero.derecha().derecha
+
+        for ficha in jugador.mano:
+
+            if (
+                ficha.izquierda == izquierda
+                or ficha.derecha == izquierda
+                or ficha.izquierda == derecha
+                or ficha.derecha == derecha
+            ):
+
+                posibles.append(ficha)
+
+        return posibles
--- partida.py
+++ partida.py
@@ -22,6 +22,15 @@ class Partida:
 
         return jugador
 
+    def crear_partida_inicial(self, nombre, fichas):
+
+        jugador = self.agregar_jugador(nombre)
+
+        for ficha in fichas:
+            jugador.añadir_ficha(ficha)
+
+        return jugador
+
     def jugador_actual(self):
 
         return self.jugadores[self.turno]
@@ -31,7 +40,6 @@ class Partida:
         self.turno += 1
 
         if self.turno >= len(self.jugadores):
-
             self.turno = 0
 
     def total_jugadores(self):
